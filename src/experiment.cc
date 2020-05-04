c68a389aff5b415a54a0c4f5901a7718e7c7c43b
Rick Boks <rickboks@hotmail.com>
2020-05-04 21:55:25 +0200

    fixed example experiment

--- src/experiment.cc
+++ src/experiment.cc
@@ -8,43 +8,28 @@ ParticleSwarm* pso;
 DifferentialEvolution* de;
 
 void algorithm
-	(std::shared_ptr<IOHprofiler_problem<double>> problem,
-		std::shared_ptr<IOHprofiler_csv_logger> logger) {
-	int const D = problem->IOHprofiler_get_number_of_variables();
-
-	pso->run(problem, logger, D*10000, D*5, std::map<int,double>()); 
+(std::shared_ptr<IOHprofiler_problem<double>> problem,
+ std::shared_ptr<IOHprofiler_csv_logger> logger) {
+    int const D = problem->IOHprofiler_get_number_of_variables();
+    pso->run(problem, logger, D*10000, D*5, std::map<int,double>()); 
 }
 
 
-void _run_experiment() {
-	pso = new ParticleSwarm(DECR_INERTIA_WEIGHT, GBEST, ASYNCHRONOUS);
-
-      std::string configName = "./configuration.ini";
-      IOHprofiler_experimenter<double> experimenter(configName,"PSO",algorithm, true); 
-      experimenter._set_independent_runs(5);
-      experimenter._run();
-      delete pso;
-}
-
-void visualize() {
-	pso = new ParticleSwarm(INERTIA_WEIGHT, GBEST, ASYNCHRONOUS);
+void _run_experiment(bool log) {
+    pso = new ParticleSwarm(DECR_INERTIA_WEIGHT, GBEST, ASYNCHRONOUS);
+    if (log)
 	pso->enableLogging();
 
-	std::string configName = "./configuration.ini";
-	IOHprofiler_experimenter<double> experimenter(configName,"PSO",algorithm, false); 
-	experimenter._set_independent_runs(5);
-	experimenter._run();
-	delete pso;
+    std::string configName = "./configuration.ini";
+    IOHprofiler_experimenter<double> experimenter(configName,"PSO",algorithm, !log); 
+    experimenter._set_independent_runs(5);
+    experimenter._run();
+    delete pso;
 }
 
 int main(int argc, char** argv){
-    _run_experiment();
-    return 0; 
-	if (argc > 1 && strcmp(argv[1], "-v") == 0){
-		visualize();	
-		return 0;
-	} else {
-		_run_experiment();
-		return 0; 
-	}
+    bool log = false;
+    if (argc > 1 && strcmp(argv[1], "-v") == 0)
+	log = true;
+    _run_experiment(log);	
 }

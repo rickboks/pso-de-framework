d40b5132ed0164b6b44a7551305aeca0c59c80c1
Rick Boks <rickboks@hotmail.com>
2021-03-04 17:37:39 +0100

    removed ycm conf

--- .ycm_extra_conf.py
+++
@@ -1,8 +0,0 @@
-import os
-
-homedir = os.getenv("HOME")
-def Settings( **kwargs ):
-  return {
-    'flags': ['-x', 'c++', 'std=c++17', '-Wall', '-Wextra', '-Wno-unused-parameter',
-      '-I', '{}/.local/include'.format(homedir), '-I', 'include', '-I', '../include' , '-lIOH']
-  }
